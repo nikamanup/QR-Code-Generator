def392941690b45f955f6e55aee5df5095515a1c
nikamanup <32682231+nikamanup@users.noreply.github.com>
2025-12-14 11:55:55 +0530

    Initial commit

---
+++ qr-code-generator/src/test/java/com/spring/practice/qr/qrcodegenerator/QrCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.practice.qr.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
